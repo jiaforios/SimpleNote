7aa5153aa83685b8abc10023f63f228c4027939f
jiaforios <jiaforios@163.com>
2018-08-20 14:13:46 +0800

    初创

---
+++ SimpleNote/Base/MZBaseNavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  MZBaseNavigationViewController.h
+//  SimpleNote
+//
+//  Created by admin on 2018/8/20.
+//  Copyright © 2018年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MZBaseNavigationViewController : UINavigationController
+
+@end
---
+++ SimpleNote/Main/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  SimpleNote
+//
+//  Created by admin on 2018/8/20.
+//  Copyright © 2018年 com. All rights reserved.
+//
+
+#import "MZBaseViewController.h"
+
+@interface MainViewController : MZBaseViewController
+
+@end
